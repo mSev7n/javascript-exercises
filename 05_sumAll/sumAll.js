b00e1c2327a9232eee7ed8a06d3bfbb92323e448
mSev7n <pythagorasm7@gmail.com>
2024-11-24 09:37:00 +0100

    made function to add sum between two arguments including the arguments themselves

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
 
+    let sum = 0;
+    for (i = int1; i <= int2; i++)
+    {
+        sum += i;
+    }
+
+    return sum
 };
+
+console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
+
