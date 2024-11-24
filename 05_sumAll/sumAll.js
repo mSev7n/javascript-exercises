3b03c8238487cf3983224bf8d4d335cd76df8af7
mSev7n <pythagorasm7@gmail.com>
2024-11-24 09:40:20 +0100

    added if statement so function works if first argument is greater than second or otherwise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,30 @@
 const sumAll = function(int1, int2) {
 
+    //variable to hold sum
     let sum = 0;
-    for (i = int1; i <= int2; i++)
-    {
-        sum += i;
+
+    //to check if first number is less than second
+    if(int1 < int2){
+        for (i = int1; i <= int2; i++)
+            {
+                sum += i;
+            
+            }
+    }
+    
+    //to check if first number is larger than second number
+    else {
+        for (i = int1; i >= int2; i--)
+        {
+            sum += i;
+        }
     }
 
+    //return sum
     return sum
 };
 
-console.log(sumAll(1,4));
+console.log(sumAll(4,1));
 
 // Do not edit below this line
 module.exports = sumAll;
