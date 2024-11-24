97e29068008acfce8bde35bd2354da537824f7e2
mSev7n <pythagorasm7@gmail.com>
2024-11-24 08:37:49 +0100

    created two functions to remove item from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+//main function to remove from array
+const removeFromArray = function(array, ...args) {
+    return array.filter(val => shouldKeepValue(val, args))
+  }
+ 
+  //function to pass into filter() call back since filter 
+  //automatically takes one argument and we migth need
+  
+  function shouldKeepValue(val, args) {
+   return !args.includes(val);
+  }
+     
 
 // Do not edit below this line
 module.exports = removeFromArray;
